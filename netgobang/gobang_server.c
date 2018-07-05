73950b2307b9edadc1534bcb9445d19cef99fc18
cg <1062831289@qq.com>
2018-07-06 01:53:11 +0800

    实现基本对战五子棋

--- netgobang/gobang_server.c
+++ netgobang/gobang_server.c
@@ -135,9 +135,9 @@ int main(int argc, char* argv[])
         return 2;
     }
 
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond, NULL);
-    pthread_cond_init(&_cond, NULL);
+    /* pthread_mutex_init(&mutex, NULL); */
+    /* pthread_cond_init(&cond, NULL); */
+    /* pthread_cond_init(&_cond, NULL); */
 
     CreateWorker(sock);
     return 0;
